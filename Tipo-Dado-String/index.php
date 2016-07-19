70f4f72c92f9ceaa224f7ab73c72bbf5402c2c4e
FernandoDantas <Fernandojrsud@hotmail.com>
2016-07-19 20:14:36 -0300

    tipo de variavel

---
+++ Tipo-Dado-String/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$curso = 'kkjkkfkdkj5565656dsjdjdjdjd';
+
+$texto = 'I\'ll go to fish';
+
+echo gettype($curso);
---
+++ Variaveis/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = 'fernando';
+$idade = '33';
+
+$Nome = 'Fernando Dantas';
+$iDade = '28';
+
+$emprego = 'Analista de sistemas';
+
+$profissaoDaPessoa = "Analista";
+
+echo 'Meu nome é ',$Nome;
